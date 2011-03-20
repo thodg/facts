ece1811c044d8b73569fb9a58abb2a59979f7ea7
Thomas de Grivel <billitch@gmail.com>
2011-03-20 03:46:08 +0100

    tests

---
+++ test.lisp
@@ -0,0 +1,34 @@
+(require :lowh-facts)
+
+(in-package :lowh-facts)
+
+(add ('result 'source ?s)
+     (?s 'id "plop")
+     (?s 'score 42))
+
+(trace replace-bindings collect-bindings% with%)
+(trace make-fact/v fact/v-subject fact/v-predicate fact/v-object)
+
+(with ((?s ?p ?o))
+  (format t "~&~S ~S ~S~&" ?s ?p ?o))
+
+(llrbtree:map-tree (lambda (key value)
+		     (format t "~&~S -> ~S~%" key value))
+		   (db-pos-tree *db*)
+		   :START (MAKE-FACT/V NIL nil NIL))
+
+(macroexpand-1
+ (third
+  (macroexpand-1
+   '(with (('result 'source ?p))
+     (format t "~S~&" ?p)))))
+
+(with (('result 'source ?p)
+       (?p 'id ?id))
+  (return (list ?p ?id)))
+
+(with (('result 'source ?p)
+       (?p 'id ?id)
+       (?p 'score ?score))
+  (format t "~S~%" (list (list ?p 'score ?score)
+			 (list ?p 'id ?id))))
