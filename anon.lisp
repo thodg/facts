7cae339e1b82209e3e3b1347f985598eb7525632
Thomas de Grivel <thoxdg@gmail.com>
2017-10-25 01:48:57 +0000

    Add optional random anon

--- anon.lisp
+++ anon.lisp
@@ -7,19 +7,28 @@
 
 ;;  Anonymous values
 
-(defpackage :facts.anon
-  (:nicknames :lowh-facts.anon))
+(defpackage :facts.anon)
 
-(defun anon (&rest name-hints)
-  (let* ((name (string-upcase (format nil "~{~A~^-~}" name-hints)))
-         (sym (intern name :facts.anon)))
-    (labels ((try (count)
-               (multiple-value-bind (s found) (intern (format nil "~A-~4,'0X"
-                                                              name count)
-                                                      :facts.anon)
+(defvar *randomize-anon* nil)
+
+(defun anon (&rest sym-hints)
+  (let* ((sym (intern
+               (with-output-to-string (out)
+                 (write-string (string-upcase (first sym-hints)))
+                 (dolist (sym-hint (rest sym-hints))
+                   (write-char #\- out)
+                   (write-string (string-upcase sym-hint))))
+               (find-package :facts.anon))))
+    (labels ((guess (count)
+               (multiple-value-bind (n found) (intern
+                                               (format nil "~A-~4,'0X"
+                                                       sym count)
+                                               :facts.anon)
                  (if found
-                     (try (1+ count))
-                     (prog1 s
+                     (if *randomize-anon*
+                         (guess (random most-positive-fixnum))
+                         (guess (1+ count)))
+                     (prog1 n
                        (setf (get sym 'anon-counter) count))))))
       (try (or (get sym 'anon-counter) 0)))))
 
