8c1cc4ea586fa33e5c1324f9abb01e821b4b2ef8
Thomas de Grivel <thoxdg@gmail.com>
2017-12-21 13:20:57 +0100

    remove old llrbtree code

--- index.lisp
+++ index.lisp
@@ -57,39 +57,3 @@
 
 (defun index-each (index fn &key start end)
   (usl-each index fn :start start :end end))
-
-#+nil(
-
-;;    LLRBTREE
-
-(defun make-index (lessp equal)
-  (declare (type (function (t t) (or t nil)) lessp equal))
-  (llrbtree:make-tree :lessp lessp))
-
-(defun index-get (index fact)
-  (declare (type fact/v fact))
-  (llrbtree:tree-get fact index))
-
-(defun index-insert (index fact)
-  (declare (type fact/v fact))
-  (setf (llrbtree:tree-get fact index) fact))
-
-(defun index-delete (index fact)
-  (declare (type fact/v fact))
-  (llrbtree:tree-delete fact index))
-
-  (defmacro index-map ((var-s var-p var-o) (index &key start end) &body body)
-  (let ((g!fact (gensym "FACT-"))
-        (g!value (gensym "VALUE-")))
-    `(llrbtree:map-tree (lambda (,g!fact ,g!value)
-                          (declare (ignore ,g!value))
-                          (let ((,var-s (fact/v-subject   ,g!fact))
-                                (,var-p (fact/v-predicate ,g!fact))
-                                (,var-o (fact/v-object    ,g!fact)))
-                            ,@body
-                            (values)))
-                        ,index
-                        ,@(when start `(:start ,start))
-                        ,@(when end   `(:end ,end)))))
-
-)
